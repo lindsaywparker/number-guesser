15f2530d23b8708fff99cf6bbdb6356839f36720
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-03-22 15:26:13 -0600

    Add last guess and button enabling

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-// Enabling
+// limit input range?
 
-var answer = Math.floor(Math.random() * 10 + 1);
-var lastGuess = "--";
+var lastGuess;
 var newGuessInput = document.getElementById('guess');
-document.getElementById('displayLastGuess').innerText = lastGuess;
-
 var newGuess = Number.parseInt(newGuessInput.value, 10);
-console.log("answer = " + answer);
+var inputMin = 1;
+var inputMax = 10;
+var answer = Math.floor(Math.random() * inputMax + 1);
+  console.log("answer = " + answer);
 
-// Check Guess ---- refresh issue???
-document.getElementById('buttonGuess').addEventListener('click',function() {
-  newGuess = newGuessInput.value;
+// Enable Check & Guess
+newGuessInput.addEventListener('keyup', function () {
+  newGuess = Number.parseInt(newGuessInput.value, 10);
+  if (newGuess <= inputMax && newGuess >= inputMin) {
+    document.getElementById('clearGuess').disabled = false;
+    document.getElementById('buttonGuess').disabled = false;
+  }
+});
+
+// Check Guess
+document.getElementById('buttonGuess').addEventListener('click', function() {
+  newGuess = Number.parseInt(newGuessInput.value, 10);
   console.log("new guess = " + newGuess);
   if (newGuess === answer) {
     var msgOutput = "BOOM!";
@@ -19,12 +28,16 @@ document.getElementById('buttonGuess').addEventListener('click',function() {
   } else {
     var msgOutput = "That is too low";
   }
+  lastGuess = newGuess;
   document.getElementById('msg').innerText = msgOutput;
+  document.getElementById('displayLastGuess').innerText = lastGuess;
+  newGuessInput.value = "";
+  document.getElementById('guess').focus();
+  document.getElementById('clearGuess').disabled = true;
+  document.getElementById('buttonGuess').disabled = true;
 });
 
 // Clear Guess Input
 document.getElementById('clearGuess').addEventListener('click', function() {
   document.getElementById('guess').innerText = "";
 });
-
-// Update Last Guess, executed every time.
