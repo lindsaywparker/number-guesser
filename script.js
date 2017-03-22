6e0059ef60be7f36152f60dde413403383805bcd
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-03-22 16:41:54 -0600

    Add input error msgs and Reset button functionality

--- script.js
+++ script.js
@@ -4,40 +4,68 @@ var lastGuess;
 var newGuessInput = document.getElementById('guess');
 var newGuess = Number.parseInt(newGuessInput.value, 10);
 var inputMin = 1;
-var inputMax = 10;
+var inputMax = 100;
 var answer = Math.floor(Math.random() * inputMax + 1);
   console.log("answer = " + answer);
 
-// Enable Check & Guess
-newGuessInput.addEventListener('keyup', function () {
-  newGuess = Number.parseInt(newGuessInput.value, 10);
-  if (newGuess <= inputMax && newGuess >= inputMin) {
-    document.getElementById('clearGuess').disabled = false;
-    document.getElementById('buttonGuess').disabled = false;
+function clearField() {
+  newGuessInput.value = "";
+  document.getElementById('clearGuess').disabled = true;
+  document.getElementById('buttonGuess').disabled = true;
+  if (lastGuess==NaN) {
+    document.getElementById('buttonReset').disabled = true;
   }
+  document.getElementById('guess').focus();
+}
+
+function errOutOfRange() {
+  alert("You've entered a number outside the specified range.  Check yo'self.");
+}
+
+function errNotNumber() {
+  alert("That's just not even a number.  Come on...");
+}
+
+// Enable all buttons
+newGuessInput.addEventListener('keyup', function () {
+  document.getElementById('clearGuess').disabled = false;
+  document.getElementById('buttonGuess').disabled = false;
+  document.getElementById('buttonReset').disabled = false;
 });
 
-// Check Guess
+// Guess Button Functionality
 document.getElementById('buttonGuess').addEventListener('click', function() {
   newGuess = Number.parseInt(newGuessInput.value, 10);
   console.log("new guess = " + newGuess);
-  if (newGuess === answer) {
-    var msgOutput = "BOOM!";
-  } else if (newGuess > answer) {
-    var msgOutput = "That is too high";
+  if (isNaN(newGuess) == true) {
+    errNotNumber();
+  } else if (newGuess > inputMax || newGuess < inputMin) {
+    errOutOfRange();
   } else {
-    var msgOutput = "That is too low";
+    if (newGuess === answer) {
+      var msgOutput = "BOOM!";
+    } else if (newGuess > answer) {
+      var msgOutput = "That is too high";
+    } else {
+      var msgOutput = "That is too low";
+    }
+    lastGuess = newGuess;
+    document.getElementById('msg').innerText = msgOutput;
+    document.getElementById('displayLastGuess').innerText = lastGuess;
   }
-  lastGuess = newGuess;
-  document.getElementById('msg').innerText = msgOutput;
-  document.getElementById('displayLastGuess').innerText = lastGuess;
-  newGuessInput.value = "";
-  document.getElementById('guess').focus();
-  document.getElementById('clearGuess').disabled = true;
-  document.getElementById('buttonGuess').disabled = true;
+  clearField();
 });
 
-// Clear Guess Input
+// Clear Button Functionality
 document.getElementById('clearGuess').addEventListener('click', function() {
-  document.getElementById('guess').innerText = "";
+  clearField();
+});
+
+// Reset Button Functionality
+document.getElementById('buttonReset').addEventListener('click', function() {
+  clearField();
+  document.getElementById('displayLastGuess').innerText = "--";
+  answer = Math.floor(Math.random() * inputMax + 1);
+    console.log("answer = " + answer);
+  document.getElementById('buttonReset').disabled = true;
 });
